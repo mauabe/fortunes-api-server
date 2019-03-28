5c62409799960ca11fb5c0df4fe4339703f0ab7b
Mauricio Feldman-Abe <mauabe@gmx.com>
2019-03-28 16:50:09 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const http= require('http');
+
+const hostname = 'localhost';
+const port = 3000;
+const server = http.createServer((req, res) => {
+  res.end('Welcom to Node server')
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at ${hostname}:${port}`);
+});
